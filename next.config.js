b318a2838a77761ea69dd204ca177718d6229fc5
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-09-28 21:30:10 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Gunakan fungsi redirects Next.js
   async redirects() {
     return [
       {
-        // Source: Rute utama (root URL)
         source: '/',
-        // Destination: Rute ke Swagger UI Anda
-        destination: '/api/docs',
-        // Permanent: 308 (digunakan untuk pengalihan permanen)
-        permanent: true, 
+        // DESTINASI BARU: /docs
+        destination: '/docs', 
+        permanent: true,
       },
     ]
   },
 }
-
 module.exports = nextConfig
