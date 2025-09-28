0fb8dc5325d4a02b89c7022c74b24f824c6783a9
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-09-28 12:34:33 +0800

    Delete src/search/pinterest.js

--- src/search/pinterest.js
+++
@@ -1,6 +0,0 @@
-// src/search/pinterest.js
-export async function pinterestSearch(q) {
-  // TODO: PASTE SIPUTZX LOGIC HERE
-  // Expected to return array of image links or object similar to siputzx
-  return { query: q, results: ["https://example.com/img1.jpg"] };
-}
