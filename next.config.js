fec0c518dfbc220517958b5156934898da69a548
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-09-28 21:51:57 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Konfigurasi redirect
   async redirects() {
     return [
       {
         source: '/',
-        // DESTINASI BARU: /docs
         destination: '/docs', 
         permanent: true,
       },
     ]
   },
+  // Pastikan ekstensi file API Routes Anda terbaca
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'], 
 }
+
 module.exports = nextConfig
