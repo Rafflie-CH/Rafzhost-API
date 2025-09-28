d50392c67e547b075d6bc6d24858ad83a638c72c
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-09-28 12:34:54 +0800

    Delete src/downloader/youtube.js

--- src/downloader/youtube.js
+++
@@ -1,5 +0,0 @@
-// src/downloader/youtube.js
-export async function youtubeDownload(url) {
-  // TODO: PASTE SIPUTZX LOGIC HERE
-  return { status: true, title: "Example YouTube", video: "https://example.com/video.mp4" };
-}
