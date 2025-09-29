5b5296a426e88de88689c030b609074a4feb4baf
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-09-29 20:29:57 +0800

    Update docs.js

--- pages/docs.js
+++ pages/docs.js
@@ -1,12 +1,24 @@
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
-import { getSwaggerSpec } from "next-swagger-doc";
+import swaggerJSDoc from "swagger-jsdoc";
 
 export default function DocsPage({ spec }) {
   return <SwaggerUI spec={spec} />;
 }
 
 export async function getStaticProps() {
-  const spec = await getSwaggerSpec(); // generate dari config next-swagger-doc.config.js
-  return { props: { spec } };
+  // Generate Swagger spec dari komentar JSDoc di pages/api
+  const swaggerSpec = swaggerJSDoc({
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Rafzhost API",
+        version: "1.0.0",
+        description: "API Documentation Rafzhost",
+      },
+    },
+    apis: ["./pages/api/**/*.js"], // semua endpoint API
+  });
+
+  return { props: { spec: swaggerSpec } };
 }
