c302fe19d1013aa2121e0daf8bdbe438080e891e
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-09-29 18:53:44 +0800

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import "@/styles/globals.css";
+import { useEffect } from "react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // Sinkron dengan tema sistem
+    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
+    const setTheme = () => {
+      document.documentElement.setAttribute(
+        "data-theme",
+        prefersDark.matches ? "dark" : "light"
+      );
+    };
+
+    setTheme();
+    prefersDark.addEventListener("change", setTheme);
+
+    return () => prefersDark.removeEventListener("change", setTheme);
+  }, []);
+
+  return <Component {...pageProps} />;
 }
-
-export default MyApp
