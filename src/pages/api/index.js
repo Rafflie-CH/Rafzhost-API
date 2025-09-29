659a6b9bfc7bafc75f42dbc9ef299bb4ad31c58e
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-09-29 18:02:23 +0800

    Update index.js

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -2,6 +2,6 @@ export default function handler(req, res) {
   res.status(200).json({
     status: true,
     message: "Rafzhost API (Vercel) running",
-    info: "Use the dashboard on /docs to test endpoints"
+    info: "Swagger Docs tersedia di /docs"
   });
 }
