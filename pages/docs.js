ea773398cd1a789e8e75353a37978ef6d52a31fa
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-09-29 21:17:41 +0800

    Update docs.js

--- pages/docs.js
+++ pages/docs.js
@@ -17,27 +17,21 @@ export default function DocsPage() {
     <div style={{ padding: "20px", maxWidth: "1200px", margin: "0 auto" }}>
       <h1 style={{ marginBottom: "20px" }}>Rafzhost API Documentation</h1>
 
-      {/* Tombol ke POST page */}
       <div style={{ marginBottom: "20px" }}>
         <Link href="/post">
-          <button className="animated">
-            Go to POST API Page
-          </button>
+          <button className="animated">Go to POST API Page</button>
         </Link>
       </div>
 
-      {/* Theme Switcher */}
       <div style={{ marginBottom: "30px", border: "1px solid #ccc", padding: "15px", borderRadius: "8px" }}>
         <h2 style={{ marginBottom: "10px" }}>Customize Theme</h2>
         <ThemeSwitcher />
       </div>
 
-      {/* Swagger UI */}
       <div ref={swaggerRef}>
-        <SwaggerUI url="/api/swagger.json" docExpansion="none" deepLinking={true} />
+        <SwaggerUI url="/api/swagger" docExpansion="none" deepLinking={true} />
       </div>
 
-      {/* Skeleton loader contoh */}
       <SkeletonLoader />
 
       <footer style={{ marginTop: "40px", textAlign: "center", color: "var(--text-color)" }}>
