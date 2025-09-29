17cc5cca9804f6922b7dc929f1dcbd4985fea7c3
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-09-30 07:56:17 +0800

    Update docs.js

--- pages/docs.js
+++ pages/docs.js
@@ -11,16 +11,28 @@ export default function DocsPage() {
     <div style={{ padding: "20px", maxWidth: "1200px", margin: "0 auto" }}>
       <h1>Rafzhost API Docs</h1>
 
+      {/* Tombol navigasi ke POST */}
       <Link href="/post">
         <button style={{ marginBottom: "20px" }}>Go to POST API Page</button>
       </Link>
 
-      <div style={{ marginBottom: "30px", border: "1px solid #ccc", padding: "15px", borderRadius: "8px" }}>
+      {/* Theme switcher */}
+      <div style={{
+        marginBottom: "30px",
+        border: "1px solid #ccc",
+        padding: "15px",
+        borderRadius: "8px"
+      }}>
         <h2>Customize Theme</h2>
         <ThemeSwitcher />
       </div>
 
-      <SwaggerUI url="/api/swagger" docExpansion="none" deepLinking={true} />
+      {/* Swagger UI load static JSON */}
+      <SwaggerUI
+        url="/swagger.json"  // <-- langsung ke public/swagger.json
+        docExpansion="none"
+        deepLinking={true}
+      />
 
       <footer style={{ marginTop: "40px", textAlign: "center" }}>
         <p>Rafzhost API &copy; {new Date().getFullYear()}</p>
