7cffd8492a7f73a3faaa040266a31ab836826581
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-09-30 07:48:17 +0800

    Update post.js

--- pages/post.js
+++ pages/post.js
@@ -3,20 +3,21 @@ import Link from "next/link";
 
 export default function PostPage() {
   return (
-    <div style={{ padding:"20px", maxWidth:"1200px", margin:"0 auto" }}>
+    <div style={{ padding: "20px", maxWidth: "1200px", margin: "0 auto" }}>
       <h1>POST API</h1>
+
       <Link href="/docs">
-        <button style={{ marginBottom:"20px" }}>Go to Docs</button>
+        <button style={{ marginBottom: "20px" }}>Go to Docs</button>
       </Link>
 
-      <div style={{ marginBottom:"30px", border:"1px solid #ccc", padding:"15px", borderRadius:"8px" }}>
+      <div style={{ marginBottom: "30px", border: "1px solid #ccc", padding: "15px", borderRadius: "8px" }}>
         <h2>Customize Theme</h2>
         <ThemeSwitcher />
       </div>
 
       {/* Tambahkan form atau info POST API di sini */}
 
-      <footer style={{ marginTop:"40px", textAlign:"center" }}>
+      <footer style={{ marginTop: "40px", textAlign: "center" }}>
         <p>Rafzhost API &copy; {new Date().getFullYear()}</p>
       </footer>
     </div>
