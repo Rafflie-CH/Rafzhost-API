03be1302cb30fd33006c18b5af8d8a6ac0a57dad
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-09-30 06:18:20 +0800

    Update post.js

--- pages/post.js
+++ pages/post.js
@@ -1,4 +1,3 @@
-// pages/post.js
 import ThemeSwitcher from "../src/components/ThemeSwitcher";
 import SkeletonLoader from "../src/components/SkeletonLoader";
 import Link from "next/link";
@@ -6,22 +5,19 @@ import Link from "next/link";
 export default function PostPage() {
   return (
     <div style={{ padding: "20px", maxWidth: "1200px", margin: "0 auto" }}>
-      <h1 style={{ marginBottom: "20px" }}>Rafzhost POST API</h1>
-
-      <div style={{ marginBottom: "20px" }}>
-        <Link href="/docs">
-          <button className="animated">Go to Docs</button>
-        </Link>
-      </div>
+      <h1>Rafzhost POST API</h1>
+      <Link href="/docs">
+        <button style={{ marginBottom: "20px" }}>Go to Docs</button>
+      </Link>
 
       <div style={{ marginBottom: "30px", border: "1px solid #ccc", padding: "15px", borderRadius: "8px" }}>
-        <h2 style={{ marginBottom: "10px" }}>Customize Theme</h2>
+        <h2>Customize Theme</h2>
         <ThemeSwitcher />
       </div>
 
       <SkeletonLoader />
 
-      <footer style={{ marginTop: "40px", textAlign: "center", color: "var(--text-color)" }}>
+      <footer style={{ marginTop: "40px", textAlign: "center" }}>
         <p>Rafzhost API &copy; {new Date().getFullYear()}</p>
       </footer>
     </div>
