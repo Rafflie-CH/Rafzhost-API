845bf900099c78fa346fe7588b2cb23f16391f37
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-09-30 20:31:29 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class", // penting biar dark mode pakai class
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/context/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
