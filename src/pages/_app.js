1c15a82d904870e56ee65f73855952b0652401da
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-09-30 20:03:20 +0800

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,30 +1,10 @@
 import "@/styles/globals.css";
-import { useEffect, useState } from "react";
+import { ThemeProvider } from "@/context/ThemeContext";
 
 export default function MyApp({ Component, pageProps }) {
-  const [theme, setTheme] = useState("system");
-
-  useEffect(() => {
-    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
-
-    const applyTheme = (t) => {
-      if (t === "system") {
-        document.documentElement.setAttribute(
-          "data-theme",
-          prefersDark.matches ? "dark" : "light"
-        );
-      } else {
-        document.documentElement.setAttribute("data-theme", t);
-      }
-    };
-
-    applyTheme(theme);
-
-    const listener = () => applyTheme(theme);
-    prefersDark.addEventListener("change", listener);
-
-    return () => prefersDark.removeEventListener("change", listener);
-  }, [theme]);
-
-  return <Component {...pageProps} theme={theme} setTheme={setTheme} />;
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
