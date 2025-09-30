560689da71cd697287d551b6ca93888c131431af
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-09-30 20:44:46 +0800

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,9 @@
+import { ThemeProvider } from "next-themes";
 import "@/styles/globals.css";
-import { ThemeProvider } from "@/context/ThemeContext";
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider>
+    <ThemeProvider attribute="class" defaultTheme="light">
       <Component {...pageProps} />
     </ThemeProvider>
   );
