d63060889fd0fbaabaeb4a0c4b59a03ba190fdb9
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-09-30 20:30:53 +0800

    Update ThemeSwitcher.js

--- src/components/ThemeSwitcher.js
+++ src/components/ThemeSwitcher.js
@@ -1,8 +1,17 @@
+"use client";
+
 import { useContext } from "react";
 import { ThemeContext } from "@/context/ThemeContext";
 
 export default function ThemeSwitcher() {
-  const { theme, setTheme } = useContext(ThemeContext);
+  const themeContext = useContext(ThemeContext);
+
+  // Aman dari error saat SSR
+  if (!themeContext) {
+    return null;
+  }
+
+  const { theme, setTheme } = themeContext;
 
   return (
     <select
@@ -10,9 +19,9 @@ export default function ThemeSwitcher() {
       onChange={(e) => setTheme(e.target.value)}
       className="px-3 py-2 rounded border bg-white dark:bg-gray-800 dark:text-white"
     >
-      <option value="system">System</option>
-      <option value="light">Light</option>
-      <option value="dark">Dark</option>
+      <option value="system">Ikuti Sistem</option>
+      <option value="light">Terang</option>
+      <option value="dark">Gelap</option>
     </select>
   );
 }
