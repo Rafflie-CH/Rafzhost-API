545e8f7d227c5cc67306b30caca617efb2d47938
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-09-30 21:23:53 +0800

    Update ThemeSwitcher.js

--- src/components/ThemeSwitcher.js
+++ src/components/ThemeSwitcher.js
@@ -1,8 +1,17 @@
 "use client";
+
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export default function ThemeSwitcher() {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
 
   return (
     <select
