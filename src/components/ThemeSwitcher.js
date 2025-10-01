ce19a371255d68865b3ce4e4ba6956479c4d9e26
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-10-01 08:07:28 +0800

    Update ThemeSwitcher.js

--- src/components/ThemeSwitcher.js
+++ src/components/ThemeSwitcher.js
@@ -1,4 +1,5 @@
 "use client";
+
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 
@@ -18,9 +19,9 @@ export default function ThemeSwitcher() {
       onChange={(e) => setTheme(e.target.value)}
       className="px-2 py-1 rounded border bg-gray-200 dark:bg-gray-700 dark:text-white"
     >
+      <option value="system">System</option>
       <option value="light">Light</option>
       <option value="dark">Dark</option>
-      <option value="system">System</option>
     </select>
   );
 }
