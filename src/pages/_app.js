19c80c86184be074665a52c70886056e8095b6bf
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-10-01 18:35:16 +0800

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,12 @@ import { ThemeProvider } from "next-themes";
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem={true}
+      disableTransitionOnChange={false}
+    >
       <Component {...pageProps} />
     </ThemeProvider>
   );
