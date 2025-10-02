4377b449ef4cc66d1183fb9c1f87d7b29a1ecab2
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-10-02 18:26:24 +0800

    Update ping.js

--- src/pages/api/ping.js
+++ src/pages/api/ping.js
@@ -2,11 +2,20 @@
  * @swagger
  * /api/ping:
  *   get:
- *     summary: Tes koneksi API
+ *     summary: Cek status API
+ *     description: Endpoint untuk mengecek apakah API hidup
  *     responses:
  *       200:
- *         description: API aktif
+ *         description: API hidup
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: "ok"
  */
 export default function handler(req, res) {
-  res.status(200).json({ message: "pong" });
+  res.status(200).json({ status: "ok" });
 }
