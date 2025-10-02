ada90f65ea7a2615e91771d993f6a17d7d2f8ec0
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-10-02 17:03:22 +0800

    Update swagger.js

--- src/pages/api/swagger.js
+++ src/pages/api/swagger.js
@@ -1,28 +1,27 @@
 // src/pages/api/swagger.js
-import swaggerJSDoc from "swagger-jsdoc";
-
-const swaggerDefinition = {
-  openapi: "3.0.0",
-  info: {
-    title: "Rafzhost API",
-    version: "1.0.0",
-    description: "Dokumentasi API Rafzhost menggunakan Swagger UI",
-  },
-  servers: [
-    {
-      url: "https://api.rafzhost.xyz/api",
-      description: "Server default Vercel",
-    },
-  ],
-};
-
-const options = {
-  swaggerDefinition,
-  apis: ["./src/pages/api/**/*.js"], // otomatis baca semua endpoint dengan swagger comment
-};
-
-const swaggerSpec = swaggerJSDoc(options);
+import swaggerJsdoc from "swagger-jsdoc";
 
 export default function handler(req, res) {
+  const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Rafzhost API",
+        version: "1.0.0",
+        description: "Dokumentasi API Rafzhost",
+      },
+      servers: [
+        {
+          url: "https://api.rafzhost.xyz", // ganti sesuai domain Vercel kamu
+        },
+      ],
+    },
+    apis: [
+      "./src/pages/api/**/*.js",   // baca semua file endpoint
+      "./src/downloader/**/*.js",  // kalau ada JSDoc di helper juga
+    ],
+  };
+
+  const swaggerSpec = swaggerJsdoc(options);
   res.status(200).json(swaggerSpec);
 }
