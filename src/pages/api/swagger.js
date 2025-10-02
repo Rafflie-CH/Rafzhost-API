f8f76d2a92c6502d6c203a0a8003ed7bf8f0faf7
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-10-02 18:53:09 +0800

    Update swagger.js

--- src/pages/api/swagger.js
+++ src/pages/api/swagger.js
@@ -1,6 +1,11 @@
-import swaggerSpec from '../../../swagger.json';
+import fs from 'fs';
+import path from 'path';
 
 export default function handler(req, res) {
+  // Path ke swagger.json di public
+  const filePath = path.join(process.cwd(), 'public', 'swagger.json');
+  const swaggerSpec = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
   res.setHeader('Content-Type', 'application/json');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.status(200).json(swaggerSpec);
