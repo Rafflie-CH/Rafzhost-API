dcdba4cca3e4d2c156ccd6ebbe907af554dc3cd2
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-10-02 18:47:56 +0800

    Create ping.js

---
+++ src/swagger-docs/ping.js
@@ -0,0 +1,18 @@
+/**
+ * @swagger
+ * /api/ping:
+ *   get:
+ *     summary: Cek status API
+ *     description: Endpoint untuk mengecek apakah API hidup
+ *     responses:
+ *       200:
+ *         description: API hidup
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: "ok"
+ */
