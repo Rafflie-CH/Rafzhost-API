0f96026bc0dfa296486df38da28e3a621d9e87df
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-10-02 18:49:46 +0800

    Update swagger.js

--- src/pages/api/swagger.js
+++ src/pages/api/swagger.js
@@ -1,31 +1,7 @@
-import swaggerJSDoc from "swagger-jsdoc";
-
-const swaggerDefinition = {
-  openapi: "3.0.0",
-  info: {
-    title: "Rafzhost API",
-    version: "1.0.0",
-    description: "📖 Dokumentasi resmi API Rafzhost",
-    contact: {
-      name: "Rafflie Aditya",
-      url: "https://api.rafzhost.xyz",
-    },
-  },
-  servers: [
-    { url: "https://api.rafzhost.xyz", description: "Production server" },
-    { url: "http://localhost:3000", description: "Local server" },
-  ],
-};
-
-const options = {
-  definition: swaggerDefinition,
-  apis: ['./src/pages/api/**/*.js'], // path relatif, sudah benar
-};
-
-const swaggerSpec = swaggerJSDoc(options);
+import swaggerSpec from '../../../swagger.json';
 
 export default function handler(req, res) {
-  res.setHeader("Content-Type", "application/json");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.status(200).send(JSON.stringify(swaggerSpec, null, 2));
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.status(200).json(swaggerSpec);
 }
