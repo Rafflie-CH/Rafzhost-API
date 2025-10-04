b8ed688cfcb4ea4b20e3bae0711b3a3572234866
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-10-04 20:13:16 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   async redirects() {
     return [
       {
         source: "/",
         destination: "/index",
-        permanent: true,
-      },
+        permanent: false
+      }
     ];
   },
 };
