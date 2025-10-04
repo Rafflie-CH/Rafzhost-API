a4611d18484cf054460f571774ae990288e156b5
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-10-04 19:38:48 +0800

    Update hello.js

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,8 +1,7 @@
 // src/pages/api/hello.js
 export default function handler(req, res) {
   if (req.method === "POST") {
-    const body = req.body || {};
-    return res.status(200).json({ ok: true, received: body });
+    return res.status(200).json({ ok: true, received: req.body || null });
   }
   return res.status(200).json({ hello: "world" });
 }
